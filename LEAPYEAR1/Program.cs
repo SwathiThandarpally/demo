9c8e5ec1f0b56e4cda19fb0871391da037ce2022
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-11 02:28:29 -0700

    Added leapyear

---
+++ LEAPYEAR1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace leapyear
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int year = 0;
+            year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a four digit year");
+
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("yes it is a leap year");
+
+            }
+            else
+            {
+                Console.WriteLine("no it is not leap year");
+            }
+
+
+        }
+    }
+}
